9f47eb96bb301883e3f2096bd82b57de1ced9a6c
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2023-03-12 23:34:54 +0100

    commit

--- src/main/java/xyz/twobtwofr/nochatreport/ChatListener.java
+++ src/main/java/xyz/twobtwofr/nochatreport/ChatListener.java
@@ -8,23 +8,39 @@ import net.kyori.adventure.text.format.Style;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ChatListener implements Listener
 {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerChat(AsyncPlayerChatEvent event)
     {
         event.setCancelled(true);
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.sendMessage("<" + event.getPlayer().getName() + "> " + (event.getMessage().startsWith(">") ? "§2" : "") + event.getMessage());
+
+        if(!Nochatreport.getInstance().getConfig().getString("chat_webhook_discord").isEmpty())
+        {
+            DiscordWebhook webhook = new DiscordWebhook(Nochatreport.getInstance().getConfig().getString("chat_webhook_discord"));
+
+            webhook.setUsername(event.getPlayer().getName());
+            webhook.setContent(event.getMessage());
+            try {
+                webhook.execute();
+            } catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
         }
-        System.out.println("<" + event.getPlayer().getName() + "> " + (event.getMessage().startsWith(">") ? "§2" : "") + event.getMessage() + "§r");
+        for(Player player : Bukkit.getOnlinePlayers())
+        {
+            player.sendMessage("<" + event.getPlayer().getName() + "> " + (event.getMessage().startsWith(">") ? "§2" : "") + event.getMessage() + "§r");
+        }
     }
 }
--- src/main/java/xyz/twobtwofr/nochatreport/Nochatreport.java
+++ src/main/java/xyz/twobtwofr/nochatreport/Nochatreport.java
@@ -14,15 +14,31 @@ public final class Nochatreport extends JavaPlugin {
     public msg message;
     public reply reply;
 
+    private static Nochatreport instance;
+
+    public static Nochatreport getInstance(){
+        return  instance;
+    }
+
     @Override
     public void onEnable()
     {
+        instance = this;
+
         message = new msg(this);
         reply = new reply(this);
+
         getServer().getPluginManager().registerEvents(new ChatListener(), this);
+
         getCommand("msg").setExecutor(message);
         getCommand("me").setExecutor(new me());
         getCommand("r").setExecutor(reply);
+
+        getConfig().addDefault("chat_webhook_discord", "");
+
+        getConfig().options().copyDefaults(true);
+
+        saveConfig();
     }
 
     @Override
